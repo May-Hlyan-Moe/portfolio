4c449c91ab19902f2a5b1fb06ce42265cdacf320
Jarvviss-Black <jarvviss@proton.me>
2025-12-14 13:09:44 +0630

    Add 'About Me' Section

--- src/About.js
+++ src/About.js
@@ -6,6 +6,43 @@ function About () {
     return (
         <div className="About container" id="about-div">
             <div className="container">
+                <div className="about-me mb-5">
+                    <h1 className="text-center fw-bold">
+                        About Me
+                        <hr />
+                    </h1>
+
+                    <p className="fw-bold">
+                        A Web Developer based in Yangon, Myanmar.
+                    </p>
+                    <a className="btn btn-dark fw-bold">
+                        <span className="fs-5 px-2">
+                            Download CV
+                        </span>
+                        <i className="bi bi-arrow-up-right px-2"></i>
+                    </a>
+                    
+                    <hr />
+
+                    <h2>
+                        3 months +
+                    </h2>
+                    <p>
+                        professional experience
+                    </p>
+
+                    <hr />
+
+                    <h2>
+                        1
+                    </h2>
+                    <p>
+                        project delivered
+                    </p>
+
+                    <hr />
+                </div>
+
                 <div className="d-flex
                                 align-items-center
                                 justify-content-center">
